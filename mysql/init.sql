933e4ff1ea42457b47c579a82e377d006935ebcf
David Atanasoski <61380566+lulata@users.noreply.github.com>
2024-06-24 21:13:02 +0200

    fixes

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,8 +1,11 @@
-DROP TABLE IF EXISTS `notes_app`;
+USE notes_app;
 
-CREATE TABLE `places` (
+DROP TABLE IF EXISTS `notes`;
+
+CREATE TABLE `notes` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `data` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE = InnoDB AUTO_INCREMENT = 12 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
